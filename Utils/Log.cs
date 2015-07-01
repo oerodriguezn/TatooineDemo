14f3a7cc1888b6d6280940e245f99e3082183cd7
oerodriguezn <rodriguez.omare@gmail.com>
2015-07-01 07:13:33 -0430

    Archivos faltantes

---
+++ Utils/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class LogUtil
+    {
+        ///TODO: Add log to entity validation errors.
+        ///TODO: Configure log4net
+        public static void Log(string Message, Exception ex = null)
+        {
+            if (ex == null)
+                log4net.LogManager.GetLogger("root").Info(Message);
+            else
+            {
+                log4net.LogManager.GetLogger("root").Error(Message, ex);
+
+                if (ex.InnerException != null)
+                {
+                    Log(Message + " InnerException ", ex.InnerException);
+                }
+            }
+        }
+    }
+}
